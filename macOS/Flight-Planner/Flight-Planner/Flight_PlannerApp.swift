0f8682a0381a72315e41e580a227a63775fffc31
Daniel Duhon <danielgduhon@gmail.com>
2022-09-14 12:20:45 -0500

    Added macOS Project

---
+++ macOS/Flight-Planner/Flight-Planner/Flight_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flight_PlannerApp.swift
+//  Flight-Planner
+//
+//  Created by Duhon, Daniel on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Flight_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
